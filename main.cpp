f2182ccb05aed245b454e92d18673186f232454b
cpistillo <cpistillo@fi.uba.ar>
2021-10-19 13:08:46 -0300

    Update enunciado

--- main.cpp
+++ main.cpp
@@ -5,11 +5,24 @@
 #include "camion.h"
 #include "camioneta.h"
 
-int main()
+using namespace std;
+
+/* Dado el ejericico de la clase pasada de herencia y polimorfismo, quiero leer 
+argumentos del programa que me indiquen el tipo de vehiculo a crear y los valores
+necesarios para inicializarlo 
+
+Por ejemplo, al correr el programa ./vehiculos:
+./vehiculos auto 10 2 3
+
+Quiero que se me cree un auto con cilindrada = 10, combustible = 2, kilometraje = 3
+
+*/
+
+int main(int argc, char *argv[])
 {
-    Auto* automovil = new Auto(10.0,2.0,3.0);
-    std::cout << automovil->valorMercado() << std::endl;
-    std::cout << automovil->coeficienteImpositivo() << std::endl;
+    Auto* automovil = new Auto(10.0f,2.0f,3.0f);
+    cout << automovil->valorMercado() << endl;
+    cout << automovil->coeficienteImpositivo() << endl;
 
     delete automovil;
 
